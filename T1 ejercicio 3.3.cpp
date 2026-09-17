01ab5555fe28b310f6c7daa63b747518f8e4cd59
jeimy-tatianaa28 <jbeltran141@uan.edu.co>
2026-09-17 09:34:57 -0500

    Add files via upload

---
+++ T1 ejercicio 3.3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   int X = 5;
+   char Y = 'c';
+   
+   cout<<"Ingrese el valor de X: "; 
+   cin>> X;
+   cout<<"Ingrese el valor de Y: ";
+   cin>> Y;
+   
+   cout<<X    <<     Y<<endl;
+   cout<< X<<Y<<endl;
+    
+
+    return 0;
+}
